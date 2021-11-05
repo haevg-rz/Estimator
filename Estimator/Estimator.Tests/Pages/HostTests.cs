590ad6e1e85e2e471d09dc40fe6d731c7648c169
Tobias Meissner <tobias.meissner@haevg-rz.de>
2021-11-05 12:54:16 +0100

    switch to moq

--- Estimator/Estimator.Tests/Pages/HostTests.cs
+++ Estimator/Estimator.Tests/Pages/HostTests.cs
@@ -1,6 +1,4 @@
-﻿using Estimator.Data.Interface;
-using Estimator.Pages;
-using Telerik.JustMock;
+﻿using Estimator.Pages;
 using Xunit;
 
 namespace Estimator.Tests.Pages
@@ -12,32 +10,15 @@ namespace Estimator.Tests.Pages
         {
             #region Assign
 
-            var host = new Host
-            {
-                Username = "host",
-                RoomId = "123456",
-                RoomManager = Samples.RoomSample.GetRoomManagerSample()
-            };
-
-            Mock.Arrange(() => host.Alert(Arg.AnyString)).DoNothing().MustBeCalled();
-            Mock.Arrange(() => host.NavigateTo(Arg.AnyString)).DoNothing().MustBeCalled();
-            Mock.Arrange(() => host.CopyToClipboard(Arg.AnyString)).DoNothing().MustBeCalled();
-            Mock.Arrange(() => host.GeneratePieChart()).DoNothing().MustBeCalled();
-
-
-            host.CloseEstimation();
-
             #endregion
 
             #region Act
 
-            host.SetDiagram();
 
             #endregion
 
             #region Assert
 
-            Assert.Equal(3, host.diagramData.Count);
 
             #endregion
         }
